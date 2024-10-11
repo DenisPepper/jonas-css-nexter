6ffafa7be7bf49c50a4a78e7e461660fb0d5ba12
DenisPepper <thech@yandex.ru>
2024-10-11 21:26:44 +0300

    Доработает секцию историй

--- src/components/story-content/story-content.jsx
+++ src/components/story-content/story-content.jsx
@@ -1,5 +1,20 @@
+import { Heading } from '../shared/heading-4/heading';
 import './story-content.scss';
 
 export function StoryContent() {
-  return <div className='story__content'>Story Content</div>;
+  return (
+    <div className='story__content'>
+      <Heading As='h2' clss='heading-2 heading-2--dark mb-md'>
+        Best desition of your life
+      </Heading>
+      <Heading As='h3' clss='heading-3 mb-sm'>
+        Happy customers
+      </Heading>
+      <p className='story__text'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur
+        voluptatibus. Quidem consequatur harum volupta!
+      </p>
+      <button className='btn'>Find your own home</button>
+    </div>
+  );
 }
--- src/components/story-pictures/story-pictures.jsx
+++ src/components/story-pictures/story-pictures.jsx
@@ -1,5 +1,12 @@
 import './story-pictures.scss';
+import imgStory1 from '/src/assets/story-1.jpeg';
+import imgStory2 from '/src/assets/story-2.jpeg';
 
 export function StoryPictures() {
-  return <div className='story__pictures'>Pictures</div>;
+  return (
+    <div className='story__pictures'>
+      <img src={imgStory1} alt='' className='story__img--1' />
+      <img src={imgStory2} alt='' className='story__img--2' />
+    </div>
+  );
 }
