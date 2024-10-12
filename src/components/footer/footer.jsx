4b0f429828ff85fb752a7c66dea2a963341c4b10
DenisPepper <thech@yandex.ru>
2024-10-12 20:13:36 +0300

    Добавит секцию футера

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,28 @@
 import './footer.scss';
 
+const links = [
+  { href: '#', text: 'Find your dream home' },
+  { href: '', text: 'Request proposal' },
+  { href: '', text: 'Download home planner' },
+  { href: '', text: 'Contact us' },
+  { href: '', text: 'Submit your property' },
+  { href: '', text: 'Come work with us' },
+];
+
 export function Footer() {
-  return <footer className='footer'>Footer</footer>;
+  return (
+    <footer className='footer'>
+      <ul className='nav'>
+        {links.map((link) => (
+          <li className='nav__item' key={link.text}>
+            <a className='nav__link' href={link.href}>
+              {link.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <p className='copyright'>&copy; Copyright 2024 by pepperLogic</p>
+    </footer>
+  );
 }
