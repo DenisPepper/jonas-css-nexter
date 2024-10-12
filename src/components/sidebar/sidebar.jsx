53782fbbbd8f7f5e4bb6b2c6504696ec7952c9ed
DenisPepper <thech@yandex.ru>
2024-10-12 20:25:42 +0300

    Добавит кнопку на боковую панель

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,5 +1,7 @@
 import './sidebar.scss';
 
 export function Sidebar() {
-  return <div className='sidebar'>Sidebar</div>;
+  return <div className='sidebar'>
+     <button className='nav-btn'></button>
+  </div>;
 }
